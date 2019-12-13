1c1762203aa3de33756aebe71b276b32215585d2
a.delcroix <a.delcroix@acti.fr>
2019-12-13 10:56:25 +0100

    Fix option general

--- Handler/OptionsHandler.php
+++ Handler/OptionsHandler.php
@@ -1,26 +1,26 @@
 <?php
 
-namespace ActiTimberPackage\Handler;
+    namespace ActiTimberPackage\Handler;
 
-final class OptionsHandler
-{
-    public function __construct()
+    final class OptionsHandler
     {
-        $this->_addGeneralOptionPage();
-    }
+        public function __construct()
+        {
+            add_action('init', [$this, 'addGeneralOptionPage']);
+        }
 
-    private function _addGeneralOptionPage()
-    {
-        /* Create ACF global theme options */
-        if (function_exists('acf_add_options_page')) {
-          /* Admin menu general settings */
-            $optionPageArgs = [
-            'page_title' => 'Options générales',
-            'menu_title' => 'Options générales',
-            'menu_slug' => 'theme-general-settings',
-            'capability' => 'edit_theme_settings'
-            ];
-            acf_add_options_page($optionPageArgs);
+        public function addGeneralOptionPage()
+        {
+            /* Create ACF global theme options */
+            if (function_exists('acf_add_options_page')) {
+                /* Admin menu general settings */
+                $optionPageArgs = [
+                    'page_title' => 'Options générales',
+                    'menu_title' => 'Options générales',
+                    'menu_slug' => 'theme-general-settings',
+                    'capability' => 'edit_theme_settings'
+                ];
+                acf_add_options_page($optionPageArgs);
+            }
         }
     }
-}
