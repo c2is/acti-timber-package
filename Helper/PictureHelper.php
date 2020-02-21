70984dd3a7d7191d277a4e4792cd616bcab19c49
a.delcroix <a.delcroix@acti.fr>
2020-02-21 15:06:15 +0100

    Add picture size max-width optional

--- Helper/PictureHelper.php
+++ Helper/PictureHelper.php
@@ -1,44 +1,47 @@
 <?php
 
-namespace ActiTimberPackage\Helper;
+    namespace ActiTimberPackage\Helper;
 
-use Timber\ImageHelper;
-use Timber\Timber;
+    use Timber\ImageHelper;
+    use Timber\Timber;
 
-final class PictureHelper
-{
-    /**
-     * Generate picture tag with image src
-     *
-     * @param $src string
-     * @param $sizes array
-     * @param $class string
-     * @param $alt string
-     * @param $retina array
-     */
-    public static function create($src, $sizes, $retina, $class = null, $alt = null, $attrs = null)
+    final class PictureHelper
     {
-        $templateLocation = WPMU_PLUGIN_DIR . '/acti-timber-package/Templates/picture/base.twig';
+        /**
+         * Generate picture tag with image src
+         *
+         * @param $src string
+         * @param $sizes array
+         * @param $class string
+         * @param $alt string
+         * @param $retina array
+         */
+        public static function create($src, $sizes, $retina, $class = null, $alt = null, $attrs = null)
+        {
+            $templateLocation = WPMU_PLUGIN_DIR . '/acti-timber-package/Templates/picture/base.twig';
 
-        $imageHelper = new ImageHelper();
-        $jpgImage = $imageHelper->img_to_jpg($src);
-        $resizes = [];
-        foreach ($sizes as $size => $args) {
-            $resizes[$size] = [
-                'min'   => $args['min'],
-                'max'   => $args['max'],
-                'src'   => $imageHelper->resize($jpgImage, $args['resize'][0], $args['resize'][1])
-            ];
+            $imageHelper = new ImageHelper();
+            $jpgImage = $imageHelper->img_to_jpg($src);
+            $resizes = [];
+            foreach ($sizes as $size => $args) {
+                $resizes[$size] = [
+                    'min'   => $args['min'],
+                    'src'   => $imageHelper->resize($jpgImage, $args['resize'][0], $args['resize'][1])
+                ];
+
+                if (isset($args['max'])) {
+                    $resizes[$size]['max'] = $args['max'];
+                }
+            }
+
+            $context = Timber::get_context();
+            $context['acti_picture_src'] = $jpgImage;
+            $context['acti_picture_sizes'] = $resizes;
+            $context['acti_picture_retina'] = $retina;
+            $context['acti_picture_class'] = $class;
+            $context['acti_picture_alt'] = $alt;
+            $context['acti_picture_attrs'] = $attrs;
+
+            Timber::render($templateLocation, $context);
         }
-
-        $context = Timber::get_context();
-        $context['acti_picture_src'] = $jpgImage;
-        $context['acti_picture_sizes'] = $resizes;
-        $context['acti_picture_retina'] = $retina;
-        $context['acti_picture_class'] = $class;
-        $context['acti_picture_alt'] = $alt;
-        $context['acti_picture_attrs'] = $attrs;
-
-        Timber::render($templateLocation, $context);
     }
-}
