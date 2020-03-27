3dffeb2c166d13177dad5abbe9110b8c85f8e67a
Adrien Delcroix <a.delcroix@acti.fr>
2020-03-27 12:53:01 +0100

    Add twig template path for acti helper

--- Handler/TwigHandler.php
+++ Handler/TwigHandler.php
@@ -5,20 +5,33 @@ namespace ActiTimberPackage\Handler;
 use DOMDocument;
 use Timber\Twig_Function;
 use Twig\Extension\StringLoaderExtension;
+use WPML\Core\Twig_Loader_Filesystem;
 
 class TwigHandler
 {
     /** Add timber support. */
     public function __construct()
     {
+        add_filter('timber/loader/loader', [$this, 'addPathToTwig']);
         add_filter('get_twig', [$this, 'addToTwig']);
     }
 
-  /** This is where you can add your own functions to twig.
-   *
-   * @param string $twig get extension.
-   * @return string
-   */
+    /**
+     * @param $loader Twig_Loader_Filesystem
+     * @return mixed
+     * @throws \WPML\Core\Twig\Error\LoaderError
+     */
+    public function addPathToTwig($loader)
+    {
+        $loader->addPath(WPMU_PLUGIN_DIR . '/acti-timber-package/Templates', 'acti_helper');
+        return $loader;
+    }
+
+    /** This is where you can add your own functions to twig.
+     *
+     * @param string $twig get extension.
+     * @return string
+     */
     public function addToTwig($twig)
     {
         $twig->addExtension(new StringLoaderExtension());
--- Helper/PictureHelper.php
+++ Helper/PictureHelper.php
@@ -1,49 +1,49 @@
 <?php
 
-    namespace ActiTimberPackage\Helper;
+namespace ActiTimberPackage\Helper;
 
-    use Timber\ImageHelper;
-    use Timber\Timber;
+use Timber\ImageHelper;
+use Timber\Timber;
 
-    final class PictureHelper
+final class PictureHelper
+{
+    /**
+     * Generate picture tag with image src
+     *
+     * @param $src string
+     * @param $sizes array
+     * @param $class string
+     * @param $alt string
+     * @param $retina array
+     * @param $attrs array
+     * @param $crop string
+     */
+    public static function create($src, $sizes, $retina, $class = null, $alt = null, $attrs = null, $crop = 'center')
     {
-        /**
-         * Generate picture tag with image src
-         *
-         * @param $src string
-         * @param $sizes array
-         * @param $class string
-         * @param $alt string
-         * @param $retina array
-         * @param $attrs array
-         * @param $crop string
-         */
-        public static function create($src, $sizes, $retina, $class = null, $alt = null, $attrs = null, $crop = 'center')
-        {
-            $templateLocation = WPMU_PLUGIN_DIR . '/acti-timber-package/Templates/picture/base.twig';
+        $templateLocation = '@acti_helper/picture/base.twig';
 
-            $imageHelper = new ImageHelper();
-            $jpgImage = $imageHelper->img_to_jpg($src);
-            $resizes = [];
-            foreach ($sizes as $size => $args) {
-                $resizes[$size] = [
-                    'min'   => $args['min'],
-                    'src'   => $imageHelper->resize($jpgImage, $args['resize'][0], $args['resize'][1], $crop)
-                ];
+        $imageHelper = new ImageHelper();
+        $jpgImage = $imageHelper->img_to_jpg($src);
+        $resizes = [];
+        foreach ($sizes as $size => $args) {
+            $resizes[$size] = [
+                'min'   => $args['min'],
+                'src'   => $imageHelper->resize($jpgImage, $args['resize'][0], $args['resize'][1], $crop)
+            ];
 
-                if (isset($args['max'])) {
-                    $resizes[$size]['max'] = $args['max'];
-                }
+            if (isset($args['max'])) {
+                $resizes[$size]['max'] = $args['max'];
             }
-
-            $context = Timber::get_context();
-            $context['acti_picture_src'] = $jpgImage;
-            $context['acti_picture_sizes'] = $resizes;
-            $context['acti_picture_retina'] = $retina;
-            $context['acti_picture_class'] = $class;
-            $context['acti_picture_alt'] = $alt;
-            $context['acti_picture_attrs'] = $attrs;
-
-            Timber::render($templateLocation, $context);
         }
+
+        $context = Timber::get_context();
+        $context['acti_picture_src'] = $jpgImage;
+        $context['acti_picture_sizes'] = $resizes;
+        $context['acti_picture_retina'] = $retina;
+        $context['acti_picture_class'] = $class;
+        $context['acti_picture_alt'] = $alt;
+        $context['acti_picture_attrs'] = $attrs;
+
+        Timber::render($templateLocation, $context);
     }
+}
